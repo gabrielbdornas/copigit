39b23c2a3691a210d89fdf9f03684cb1fccc514d
Gabriel Dornas <gabrielbdornas@gmail.com>
2025-10-10 15:19:44 -0300

    Test commit initial message existence
    After adding a initial commit to the new projects
    See #14

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,5 +1,6 @@
 from tests.conftest import project_dir
 
+import subprocess
 import pytest
 
 def test_git_creation(create_project):
@@ -12,3 +13,25 @@ def test_git_creation(create_project):
     project_path_structure = _project_dir[3]
 
     assert '.git' in project_path_structure
+
+
+def test_initial_commit_created(create_project):
+    """
+    Test that an initial commit exists in the newly created project.
+    """
+    project = create_project()
+    _project_dir = project_dir(project)
+    project_path = _project_dir[1][0]
+
+    # Try to get the latest commit message
+    result = subprocess.run(
+        ["git", "-C", project_path, "log", "-1", "--pretty=%s"],
+        capture_output=True,
+        text=True,
+    )
+    
+    commit_message = result.stdout.strip()
+
+    assert "Initial" in commit_message or "init" in commit_message.lower(), (
+        f"Unexpected initial commit message: {commit_message!r}"
+    )
